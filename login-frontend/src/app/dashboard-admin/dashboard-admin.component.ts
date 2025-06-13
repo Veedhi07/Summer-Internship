c0d5a157fcae2432b64fd9c50d45641f1e24589f
Veedhi Sharma <veedhi2020@gmail.com>
2025-06-13 19:25:23 +0530

    Delete login-frontend directory

--- login-frontend/src/app/dashboard-admin/dashboard-admin.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-admin-dashboard',
-  standalone: true,
-  templateUrl: './dashboard-admin.component.html',
-  styleUrls: ['./dashboard-admin.component.css']
-})
-export class AdminDashboardComponent {}
--- login-frontend/src/app/dashboard-user/dashboard-user.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-dashboard',
-  standalone: true,
-  templateUrl: './dashboard-user.component.html',
-  styleUrls: ['./dashboard-user.component.css']
-})
-export class UserDashboardComponent {}
