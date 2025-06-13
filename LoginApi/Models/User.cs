85e7ef1739998c9c4cb34fcf27114744f51e36ec
Veedhi Sharma <veedhi2020@gmail.com>
2025-06-13 19:25:05 +0530

    Delete LoginApi directory

--- LoginApi/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿public class User
-{
-    public int Id { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; } // Store hashed password
-    public string Role { get; set; }
-}
