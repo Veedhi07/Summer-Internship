cd0d3ebc22ed5be0aad73ff6cf901dca3dadf331
Veedhi Sharma <veedhi2020@gmail.com>
2025-06-13 18:59:09 +0530

    Delete query2.sql

--- query2.sql
+++
@@ -1,7 +0,0 @@
-select * from "Books"
-
-Insert into "Books"("Title","Author","Genre") values('2 states','Chetan Bhagat','Fiction');
-
-Insert into "Books"("Title","Author","Genre") values('Atomic  Habits','James Bond','Fiction');
-
-select * from "Books"
